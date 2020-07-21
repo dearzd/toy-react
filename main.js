446f61a1abbba2c7c13e4618e9e355d97d9d362f
dearzd <1147433303@qq.com>
2020-07-21 22:18:48 +0800

    toy react v2

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
 import {ToyReact, Component} from './ToyReact';
 
-class MyComponent extends Component{
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
+            <button
+                className="square"
+                onClick={() => this.setState({value: 'X'})}
+            >
+                {this.state.value || ''}
+            </button>
+        );
+    }
+}
+
+class Board extends Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
             <div>
-                <span>hello</span>
-                <span>world!</span>
-                <div>
-                    {this.children}
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
@@ -15,10 +52,8 @@ class MyComponent extends Component{
 }
 
 let a = (
-    <MyComponent name="a" id="ida" >
-        {true}
-        <div>hahaha</div>
-    </MyComponent>
+    <Board />
 );
 
-ToyReact.render(a, document.body);
+console.log(document.getElementById('app'));
+ToyReact.render(a, document.getElementById('app'));
